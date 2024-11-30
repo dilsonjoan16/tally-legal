f59ade1502405495e99e149f591350bbd79d44ab
dilsonjoan16 <dilsonjoan16@gmail.com>
2024-11-30 13:54:48 -0400

    Add profile module.
    Fix #7.

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ProfileController extends Controller
+{
+    public function __construct(public Profile $model)
+    {
+        parent::__construct();
+        $this->model = new Profile;
+    }
+
+    public function showProfile()
+    {
+        return response()->json([
+            'profile' => auth()->user()->profile
+        ]);
+    }
+
+    public function storeProfile(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'phone' => 'required|string|min:7|max:15|unique:profiles,phone',
+            'address' => 'required|string',
+            'city' => 'nullable|string',
+            'state' => 'nullable|string',
+            'country' => 'nullable|string',
+            'zip' => 'nullable|string',
+        ]);
+
+        try {
+            $user = auth()->user();
+
+            $request->merge([
+                'user_id' => $user->id
+            ]);
+
+            // Handle transaction.
+            $user->profile()->create($request->all());
+
+            return response()->json([
+            'message' => 'Profile created',
+            'profile' => $user->profile
+        ], 201);
+        } catch (\Throwable $th) {
+            Log::error('Error creating profile', ['error' => $th]);
+            return response()->json([
+                'message' => 'Profile not created',
+            ], 500);
+        }
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $user = auth()->user();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        if (!$user->profile) {
+            return response()->json([
+                'message' => 'Profile not found',
+            ], 404);
+        }
+
+        $request->validate([
+            'first_name' => 'nullable|string',
+            'last_name' => 'nullable|string',
+            'phone' => 'nullable|string|unique:profiles,phone,' . $user->profile->id,
+            'address' => 'nullable|string',
+            'city' => 'nullable|string',
+            'state' => 'nullable|string',
+            'country' => 'nullable|string',
+            'zip' => 'nullable|string',
+        ]);
+
+        try {
+            // Handle transaction.
+            $user->profile()->update($request->all());
+
+            return response()->json([
+                'message' => 'Profile updated',
+                'profile' => $user->profile
+            ], 201);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return response()->json([
+                'message' => 'Profile not updated',
+            ], 500);
+        }
+    }
+
+    public function manageAvatar(Request $request)
+    {
+        $request->validate([
+            'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $user = auth()->user();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        if (!$user->profile) {
+            return response()->json([
+                'message' => 'Profile not found',
+            ], 404);
+        }
+
+        try {
+            $user->profile->avatar = $this->saveRemoteAssets->__invoke($request, $user->profile->avatar, $this->model->getTable(), 'avatar');
+            $user->profile->save();
+
+            return response()->json([
+                'message' => 'Avatar updated',
+                'profile' => $user->profile
+            ], 201);
+        } catch (\Throwable $th) {
+            Log::error('Avatar not updated.', ['error' => $th->getMessage()]);
+            return response()->json([
+                'message' => 'Avatar not updated',
+            ], 500);
+        }
+    }
+}
---
+++ app/Models/Profile.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'avatar',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'country',
+        'zip',
+    ];
+
+    /**
+     * The user that owns the profile.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
